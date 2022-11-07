9bc6056874e98f20d6169fbb3270a2cb49c998b4
ReptiloidMark <113239517+ReptiloidMark@users.noreply.github.com>
2022-11-07 19:04:51 +0300

    Add exercises

---
+++ homework2/BinarySearch.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int Binary_search(vector<int>x, int target) {
+    int maximum = (x.size()) - 1;
+    int minimum = 0;
+    int mean;
+    while (maximum > minimum) {
+        mean = (maximum + minimum) / 2;
+        if (x[mean] == target) {
+            return true;
+        }
+        else if (x[mean] > target) {
+            maximum = (mean - 1);
+        }
+        else {
+            minimum = (mean + 1);
+        }
+    }
+    return false;
+}
+int main() {
+    unsigned int i;
+    int n;
+    vector<int>x;
+    cout << "Enter the amount of numbers you want to evaluate: ";
+    cin >> i;
+    cout << "Enter your numbers to be evaluated: " << endl;
+    while (x.size() < i && cin >> n) {
+        x.push_back(n);
+    }
+    int target;
+    cout << "Enter the target you want to search for in the selected array \n";
+    cin >> target;
+    if (Binary_search(x, target)) {
+        cout << "Your result is found!";
+    }
+    else {
+        cout << "Your result is not found!";
+    }
+    return 0;
+}
---
+++ homework2/InsertionSort.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+void insertionSort(vector<int> data, int n)
+{
+    int i, j, tmp;
+
+    for (i = 1; i < n; i++)
+    {
+        j = i;
+        tmp = data[i];
+        while (j > 0 && tmp < data[j - 1])
+        {
+            data[j] = data[j - 1];
+            j--;
+        }
+        data[j] = tmp;
+    }
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    vector<int> numberVectors;
+    cout << "Введите размер массива: ";
+    int n;
+    cin >> n;
+    cout << "Введите числа через Enter: ";
+    int temp;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> temp;
+        numberVectors.push_back(temp);
+
+    }
+    insertionSort(numberVectors, n);
+    for (auto e : numberVectors)
+    {
+        std::cout << e << " ";
+    }
+}
+
